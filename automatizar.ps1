5ebdc2765bf6bd3aaf6e13dc7a3ee517b2f2d2e8
juliansalcedo19 <55834899+juliansalcedo19@users.noreply.github.com>
2020-02-28 17:49:55 +0100

    Add files via upload

---
+++ automatizar.ps1
@@ -0,0 +1,67 @@
+﻿$MouseEventSig=@'
+[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+'@
+$MouseEvent = Add-Type -memberDefinition $MouseEventSig -name "MouseEventWinApi" -passThru
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(329,62)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(592,337)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 6
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1354,12)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(587,361)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(587,361)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+#Write some text
+[System.Windows.Forms.SendKeys]::SendWait("Cascos")
+#Press on Enter
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep 7
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(1354,12)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(801,229)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(801,229)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 4
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(585,379)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+#Write some text
+[System.Windows.Forms.SendKeys]::SendWait("Me encanta automatizar")
+#Press on Enter
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+
+Start-Sleep 6
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(801,229)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
+
+Start-Sleep 3
+[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(801,229)
+$MouseEvent::mouse_event(0x00000002, 0, 0, 0, 0)
+$MouseEvent::mouse_event(0x00000004, 0, 0, 0, 0)
---
+++ formulariofinal.ps1
@@ -0,0 +1,110 @@
+﻿using assembly System.Windows.Forms
+using namespace System.Windows.Forms
+$Form  = New-Object System.Windows.Forms.Form
+
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "aplicacion"
+$Label.AutoSize = $True
+$Form.MinimizeBox = $False
+$Form.MaximizeBox = $True
+$Form.StartPosition = 'CenterScreen'
+$button = [Button] @{
+ Text = 'enlace a pagina '
+}
+$button1 = [Button] @{
+ Text = 'pagina  '
+ 
+}
+$button2 = [Button] @{
+ Text = 'comentaios'
+ 
+}
+$button3 = [Button] @{
+ Text = 'buscar producto '
+ 
+}
+
+
+$button.Location = New-Object System.Drawing.Point (20,80)
+$button.Size= New-Object System.Drawing.Size(100,20)
+$button1.Location = New-Object System.Drawing.Point (20,100)
+$button2.Location = New-Object System.Drawing.Point (20,120)
+$button3.Location = New-Object System.Drawing.Point (20,100)
+$button3.Size= New-Object System.Drawing.Size(100,20)
+$button4.Location = New-Object System.Drawing.Point (140,100)
+$TextBox = New-Object System.Windows.Forms.TextBox
+$TextBox.Location = New-Object System.Drawing.Size(20,220)
+$TextBox.Size = New-Object System.Drawing.Size(260,20)
+
+$Caja = New-Object System.Windows.Forms.TextBox
+$Caja.Location = New-Object System.Drawing.Size(20,220)
+$Caja.Size = New-Object System.Drawing.Size(260,20)
+
+
+$button.add_Click{
+
+Start-Process "https://www.pccomponentes.com/"
+
+
+}
+$button1.add_Click{
+
+$Form = New-Object System.Windows.Forms.Form
+$Label = New-Object System.Windows.Forms.Label
+
+$Label.Text = "hola bienvenido a nuestra pagina"
+$Label.AutoSize = $True
+$Form.StartPosition = 'CenterScreen'
+$Form.Controls.Add($Label)
+$Form.ShowDialog($form.Controls.Add($button3))
+
+
+
+}
+
+$button2.add_Click{
+
+
+$Form = New-Object System.Windows.Forms.Form
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "añadir comentarios"
+$Label.AutoSize = $True
+$Form.StartPosition = 'CenterScreen'
+$Form.Controls.Add($Label)
+$Form.KeyPreview = $True
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Enter"){$Var=$TextBox.Text;Write-Host "el comentario es " $Var;$Form.Close()}})
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Escape"){$Form.Close()}})
+$Form.ShowDialog($Form.Controls.Add($TextBox))
+
+
+}
+
+$button3.add_Click{
+
+
+$Form = New-Object System.Windows.Forms.Form
+$Label = New-Object System.Windows.Forms.Label
+$Label.Text = "introducir producto"
+$Label.AutoSize = $True
+$Form.StartPosition = 'CenterScreen'
+$Form.Controls.Add($Label)
+$Form.KeyPreview = $True
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Enter"){$Var=$Caja.Text;Start-Process https://www.pccomponentes.com/buscar/?query=$Var;
+Start-Process https://www.google.com/search?q=$Var;
+Start-Process https://www.amazon.es/s?k=$Var}})
+$Form.Add_KeyDown({if ($_.KeyCode -eq "Escape"){$Form.Close()}})
+
+$Form.ShowDialog($Form.Controls.Add($Caja))
+
+
+
+}
+
+
+$form.Controls.Add($button)
+$form.Controls.Add($button1)
+$form.Controls.Add($button2)
+$form.controls.add($pictureBox)
+$Form.Controls.Add($Label)
+
+$Form.ShowDialog()
