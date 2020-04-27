d23c4a523a58f1978094581aa7a8a76d23b3e80e
juliansalcedo19 <55834899+juliansalcedo19@users.noreply.github.com>
2020-04-27 13:54:23 +0200

    Add files via upload

---
+++ entrega27abril/SchemaDB.java
@@ -0,0 +1,10 @@
+package entrega27abril;
+
+public interface SchemaDB {
+    String NOM_DB="entornos";
+    String NOM_TABLA="datos";
+    String COL_NOM="nombre";
+    String COL_APE="apellido";
+    String COL_EDAD="edad";
+    String COL_TEL="telefono";
+}
